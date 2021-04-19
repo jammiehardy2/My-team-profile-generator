c6aa8182a058a7d47a95a059a2553a8f8449e37d
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2021-04-19 11:04:54 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -13,10 +13,40 @@ const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
-const teamMember=[];
-function app{}{
-    function getManager{}{
-        inquirer.prompt([
+const teamMember=[
+    promptUser();
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+            type:"input",
+            name:"employeeName",
+            message:"What is your name"
+      },
+    {
+            type:"input",
+            name:"employeeId",
+            message:"What is your Id"
+    },
+    {
+        type:"input",
+        name:"employeeEmail",
+        message:"What is your work Email"
+    },
+    {
+        type:"input",
+        name:"officeNumber",
+        message:"What is your number"
+    }
+    ]).then(response => {
+            const employee = new Employee(response.employeeName,response.employeeId,response.memployeeEmail,response.officeNumber);
+            teamMember.push(employee);
+            addingNewMember(); 
+        })
+        promptUser();
+        function promptUser() {
+          inquirer
+            .prompt([
   {
         type:"input",
         name:"managerName",
@@ -37,13 +67,15 @@ function app{}{
     name:"officeNumber",
     message:"What is your number"
 }
-    }).then(response => {
+]).then(response => {
         const manager = new Manager(response.managerName,response.managerId,response.managerEmail,response.officeNumber);
         teamMember.push(manager);
         addingNewMember(); 
     })
-    function getEngineer{}{
-        inquirer.prompt([
+    promptUser();
+    function promptUser() {
+      inquirer
+        .prompt([
   {
         type:"input",
         name:"engineerName",
@@ -64,13 +96,15 @@ function app{}{
     name:"github",
     message:"What is your Github name"
 }
-    }).then(response => {
+]).then(response => {
         const engineer = new Engineer(response.engineerName,response.engineerId,response.engineerEmail,response.github);
         teamMember.push(engineer);
         addingNewMember(); 
     })
-    function getIntern{}{
-        inquirer.prompt([
+    promptUser();
+function promptUser() {
+  inquirer
+    .prompt([
   {
         type:"input",
         name:"internName",
@@ -91,31 +125,47 @@ function app{}{
     name:"school",
     message:"What school did you attend"
 }
-    }).then(response => {
+]).then(response => {
         const intern = new Intern(response.internName,response.internId,response.internEmail,response.school);
         teamMember.push(intern);
         addingNewMember(); 
-    })      
-        ])
-}
-// and to create objects for each team member (using the correct classes as blueprints!)
+    });  
+    .then((data) => {
+      const secondreadme = `${data.title.toLowerCase().split(" ").join("")}.md`;
+      createFile(secondreadme, createMD(data));
+      console.log(data);
+    });
+}    
+    function createFile(file, data) {
+        fs.writeFileSync(path.join(process.cwd(), file), data);
+        console.log("Success!");
+      }
+      
+      function createMD(data) {
+        // return
+        //Adding licences and their function?
+        //[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+        //[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+        //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+        //[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+        return `
+      ## Name
+        ${data.title}
+      ## Table of contents
+      *[employee](#employee)
+      *[manager](#manager)
+      *[engineer](#engineer)
+      *[intern](#intern)
+      * [github](#github)
+      * [email](#email)
+      ## github
+      ${data.github}
+      
+      ## email
+      ${data.email}
+      
+      ## license
+      ${data.license}`;
+      }
+        
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
