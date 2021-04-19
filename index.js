4b8441ea2c8eb55cc5f5b314bc12851346bb44b0
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2021-04-19 11:39:38 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -21,24 +21,24 @@ function promptUser() {
       {
         type: "input",
         name: "projectName",
-        message: "What is the name of your project?",
+        message: "What is your title?",
       },
-      {
-        type: "input",
-        name: "description",
-        message: "Explain a bit about your project",
-      },
+      // {
+      //   type: "input",
+      //   name: "description",
+      //   message: "Explain a bit about your project",
+      // },
 
       {
         type: "input",
         name: "installation",
         message: "Explain installation instructions",
       },
-      {
-        type: "input",
-        name: "usage",
-        message: "What is your project used for?",
-      },
+      // {
+      //   type: "input",
+      //   name: "usage",
+      //   message: "What is your project used for?",
+      // },
       {
         type: "input",
         name: "email",
@@ -75,30 +75,17 @@ function createMD(data) {
   //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
   //[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
   return `
-## Name
+  ## Name
   ${data.title}
 ## Table of contents
+*[employee](#employee)
+*[manager](#manager)
+*[engineer](#engineer)
+*[intern](#intern)
 * [github](#github)
-* [projectName](#projectName)
-* [description](#description)
-* [installation](#installation)
-* [usage](#usage)
 * [email](#email)
-* [license](#license)
 ## github
 ${data.github}
-
-## projectName
-${data.projectName}
-
-## description
-${data.description}
-
-## installation
-${data.installation}
-
-## usage
-${data.usage}
 
 ## email
 ${data.email}
